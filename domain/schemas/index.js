2a43cace308508ddb61ffadd36f357e980a2ff05
zVictor <victor0110@msn.com>
2016-09-02 17:04:43 +0200

    remove Brand object

--- domain/schemas/index.js
+++ domain/schemas/index.js
@@ -1,12 +1,10 @@
 import Author from './Author.graphql';
-// import Brand from './Brand.graphql';
 import Post from './Post.graphql';
 import Query from './Query.graphql';
 import schema from './schema.graphql';
 
 export default [
   Author,
-  // Brand,
   Post,
   Query,
   schema,
