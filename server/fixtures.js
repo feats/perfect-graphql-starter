d12e5a46148ef44b3b16f7dcb335f81594cc62f2
zVictor <victor0110@msn.com>
2016-09-02 16:19:58 +0200

    replace fixtures by related content

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,15 +3,15 @@ module.exports = {
     connection.then(connector => {
       connector.collection('authors').deleteMany({}).then(() => {
         connector.collection('authors').insertMany([
-          { _id: 1, firstName: 'Dilma', lastName: 'Rousseff' },
-          { _id: 2, firstName: 'Michel', lastName: 'Temer' },
+          { _id: 1, firstName: 'Salvador', lastName: 'Dali' },
+          { _id: 2, firstName: 'Vince', lastName: 'Lombardi' },
         ]);
       });
 
       connector.collection('posts').deleteMany({}).then(() => {
         connector.collection('posts').insertMany([
-          { _id: 1, author: { _id: 1 }, title: 'It is a coup!', text: 'I did not commit a crime', views: 20 },
-          { _id: 2, author: { _id: 2 }, title: 'Whatever...', text: 'let\'s just pretend you did', views: 61 },
+          { _id: 1, author: { _id: 1 }, title: 'Perfection fear', text: 'Have no fear of perfection, you’ll never reach it.', views: 20 },
+          { _id: 2, author: { _id: 2 }, title: 'Catch excellence', text: 'Perfection is not attainable, but if we chase perfection we can catch excellence.', views: 61 },
         ]);
       });
     }).catch(err => {
